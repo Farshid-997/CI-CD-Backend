5c47da5755c04c3075f2663b5c100c7c6eb04ac5
Farshid-997 <arnob.farshid@gmail.com>
2023-08-23 17:22:13 +0600

    added

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
+
 const app = express();
-app.get("/", (req, res) => res.json({ message: "Hello World " }));
-app.listen(3000);
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello Unive!",
+  });
+});
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
